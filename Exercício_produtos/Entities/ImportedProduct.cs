5b8243571a8027adb1f3e079f09ff0eaf5b43311
murilo <murilo.santone@hotmail.com>
2021-07-11 20:20:34 -0300

    Curso CSharp - Exercício de fixação, utilizar herança e polimorfismo feito programa para ler quantos produtos quiser.

---
+++ Exercício_produtos/Entities/ImportedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício_produtos.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee)
+            : base (name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} $ {Price.ToString("F2", CultureInfo.InvariantCulture)} ({CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
